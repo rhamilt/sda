d4d40f598c940222b4c6651af13a9ef77d9b39d4
Reed Hamilton <rhamilt@uw.edu>
2024-05-30 13:39:35 -0700

    Add basic frontend support for streaks

--- web/src/ModeSelector.jsx
+++ web/src/ModeSelector.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 
 import TopKSelector from './TopKSelector'
+import StreakSelector from './StreakSelector'
 
 const ModeSelector = ({ user, limit, onSelect }) => {
   const [analysisType, setAnalysisType] = useState("TopK")
@@ -13,10 +14,16 @@ const ModeSelector = ({ user, limit, onSelect }) => {
           id="analysisType"
           onChange={(e) => setAnalysisType(e.target.value)}>
           <option value="TopK">Top K</option>
+          <option value="Streak">Streak</option>
         </select>
       </div>
       <hr/>
       {analysisType === "TopK" && <TopKSelector
+                                    onSelect={onSelect}
+                                    user={user}
+                                    limit={limit} />}
+
+      {analysisType === "Streak" && <StreakSelector
                                     onSelect={onSelect}
                                     user={user}
                                     limit={limit} />}
